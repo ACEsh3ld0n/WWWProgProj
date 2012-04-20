7be4fd2524e464b3eee5cd68aef844ab8a0edd2a
ACEsh3ld0n <legendkoala@gmail.com>
2012-04-20 19:01:00 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WWWProgProj::Application.config.secret_token = '94ad3094aa89148e872a1f46a69db63bfe6077d001e6adf49a1e86ff63a55f501448bee70d1ed94582057a8dae16f747585c98537ec464ad6cd33b2bdbeb9dec'
